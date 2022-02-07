90e010729f32097e4507254beba1c4e0cf6c82ea
Peter Conley <peterdavidconley@gmail.com>
2022-02-07 14:47:28 -0500

    wrote scaffolding for all 5 endpoints

--- api/server.js
+++ api/server.js
@@ -6,8 +6,34 @@ const server = express();
 
 server.use(express.json())
 
-server.get('/', (req, res) => {
-    res.json('Resting server get')
+// When the client makes a `POST` request to `/api/users`:
+
+server.post('/api/users', (req, res) => {
+    console.log('POST request')
+})
+
+// When the client makes a `GET` request to `/api/users`
+
+server.get('/api/users', (req, res) => {
+    res.json('GET request for all users')
+})
+
+// When the client makes a `GET` request to `/api/users/:id`
+
+server.get('/api/users/:id', (req, res) => {
+    res.json('GET request for user by id')
+})
+
+// When the client makes a `DELETE` request to `/api/users/:id`:
+
+server.delete('/api/users/:id', (req, res) => {
+    res.json('Delete request for specific user')
+})
+
+// When the client makes a `PUT` request to `/api/users/:id`:
+
+server.put('/api/users/:id', (req, res) => {
+    res.json('Update specific user')
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
